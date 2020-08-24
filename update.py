28492097896ad4abf7a13c8b5e4f87ddf51f392e
Mike Wolfram <mwolfram@gw-semi.com>
2020-08-24 22:06:37 +0200

    Change URL to clone from official FreeRTOS. Add updating the submodules.

--- update.py
+++ update.py
@@ -21,7 +21,7 @@ source_paths = [
 # clone the repository
 print("Cloning FreeRTOS repository...")
 if not Path("freertos_src").exists():
-    subprocess.run("git clone https://github.com/cjlano/freertos.git freertos_src", shell=True)
+    subprocess.run("git clone --recurse-submodules https://github.com/freertos/freertos.git freertos_src", shell=True)
 
 # remove the sources in this repo
 if Path("FreeRTOS").exists():
